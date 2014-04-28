e0632e8e072ac9b95797b6ca37fc240b80297c75
Tim Clark <timeeeee@gmail.com>
2014-04-28 13:49:38 -0500

    Sound class updates textgrid, insertSpaces doesn't crash with integer input

--- src/Sound.py
+++ src/Sound.py
@@ -7,14 +7,34 @@ class Sound:
     def __init__(self, soundPath, textgridPath):
         self.soundPath = soundPath
         self.textgrid = tgt.read_textgrid(textgridPath)
-    def pauseInsertion(self, index, length):
+    def pauseInsertion(self, index, duration):
         """
         This will get the time of the end of the word from
         the textgrid file, insert the silence at that point
         in the wave file using insertSpaces, and rewrite
         the textgrid file to reflect the change.
         """
-        startTime = self.textgrid.get_tier_by_name('words')[0]
+        wordTier = self.textgrid.get_tier_by_name('words')
+        wordIntervals = filter(lambda x: x.text != "sil" and x.text != "sp", wordTier)
+        silenceStart = wordIntervals[index].end_time
+        
+        # insert silence into wave
+        # insertSpaces.insertSilence(self.soundPath, silenceStart, duration)
+        
+        # insert silence into textgrid
+        for tier in self.textgrid:
+            print("processing tier")
+            # Shift all later intervals
+            for interval in tier:
+                print("processing interval")
+                if interval.start_time > silenceStart:
+                    interval.end_time += duration
+                    interval.start_time += duration
+            
+        tier.get_annotation_by_start_time(silenceStart).end_time += duration
+
+        # Save grid
+        tgt.write_to_file(self.textgrid, "out.TextGrid", format = "long")
 
     def pitchMod(self, startTime, length, shift):
         '''
@@ -37,3 +57,7 @@ class Sound:
         We haven't talked about how to implement this one.
         '''
         pass
+
+if __name__ == "__main__":
+    s = Sound("example.wav", "example.TextGrid")
+    s.pauseInsertion(1,1)
--- src/insertSpaces.py
+++ src/insertSpaces.py
@@ -3,10 +3,10 @@ import pydub
 def insertSilence(filename, gaptime, gaplength):
 	import pydub
 	file = pydub.AudioSegment.from_wav(filename)
-	silence = file[:gaplength * 1000]
+	silence = file[:int(gaplength * 1000)]
 	silence = silence - 50000
-	first = file[:gaptime * 1000]
-	last = file[gaptime * 1000:]
+	first = file[:int(gaptime * 1000)]
+	last = file[int(gaptime * 1000:)]
 	newfile = first + silence + last
 	newfile.export("processedfile.wav", format="wav")
 	return newfile
