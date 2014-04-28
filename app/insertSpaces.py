f3945ce216e2adafe8c5ae5434c7ad4d8879bcbf
Tim Clark <timeeeee@gmail.com>
2014-04-28 13:58:06 -0500

    unbreaking insertSpaces.py

--- app/insertSpaces.py
+++ app/insertSpaces.py
@@ -6,7 +6,7 @@ def insertSilence(filename, gaptime, gaplength):
 	silence = file[:int(gaplength * 1000)]
 	silence = silence - 50000
 	first = file[:int(gaptime * 1000)]
-	last = file[int(gaptime * 1000:)]
+	last = file[int(gaptime * 1000):]
 	newfile = first + silence + last
 	newfile.export("processedfile.wav", format="wav")
 	return newfile
