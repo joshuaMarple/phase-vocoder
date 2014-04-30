c1db6cbba2a9ce45b2d10cdf01c581bca475d953
pgowdy <pgowdy1@gmail.com>
2014-04-29 23:14:17 -0500

    changeVolume function added.

---
+++ app/changeVolume.py
@@ -0,0 +1,12 @@
+from lib import pydub
+
+def changeVolume(filename, gaptime, gaplength, decibel):
+    import pydub
+    file = pydub.AudioSegment.from_wav(filename)
+    segment = file[:int(gaplength * 1000)]
+    segment = segment + decibel
+    first = file[:int(gaptime * 1000)]
+    last = file[int(gaptime * 1000):]
+    newfile = first + segment + last
+    newfile.export("processedfile.wav", format="wav")
+    return newfile
