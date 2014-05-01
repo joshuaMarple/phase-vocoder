cf8520bba6617cbb36fb52b1f21ca32f5c338a17
Tim Clark <timeeeee@gmail.com>
2014-05-01 15:33:16 -0500

    remove soundMod.py

--- app/soundMod.py
+++
@@ -1,8 +0,0 @@
-from lib import pydub
-
-def changeVolume(filename, decibel):
-    import pydub
-    file = pydub.AudioSegment.from_wav(filename)
-    newfile = file + decibel
-    newfile.export("processedfile.wav", format="wav")
-    return newfile
