5a3f8a4deb7fb055183105c2bcb848fd42babc15
Fernando Gomes <fernando60794@hotmail.com>
2014-05-05 16:50:41 -0500

    fixed volume.py

--- app/Volume.py
+++ app/Volume.py
@@ -14,15 +14,16 @@ filename (string): the path to the soundfile
 gaptime (float): the time to begin changing the volume
 gapduration (float): the amount of time that sound will be changed(from the gaptime start to the end of this length)
 decibel (integer): the number of decibels to chang ethe volume by(from negative number to positive number) 
-Outputs: saves results to the path given by filename
+Outputs: processefile.wav
 Description: This function will change the volume of a soundfile
 """
 
 def changeVolume(filename, gaptime, gaplength, decibel):
     file = pydub.AudioSegment.from_wav(filename)
-    segment = file[:int(gaplength * 1000)]
+    segment = file[int((gaptime* 1000)) : int(((gaptime+gaplength) * 1000))]
     segment = segment + decibel
     first = file[:int(gaptime * 1000)]
-    last = file[int(gaptime * 1000):]
+    last = file[int((gaptime+gaplength) * 1000):]
     newfile = first + segment + last
     newfile.export(filename, format="wav")
+    return newfile
